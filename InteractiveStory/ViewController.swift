307cad2e5952e12d5b2956643aef302cccb96fa5
Jordi Gamez <jordigamezllorach@gmail.com>
2016-08-04 10:03:02 +0200

    Story navigation completed

--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -13,15 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        /*
-        let story = Page(story: .TouchDown)
-        story.firstChoice = (title: "some Title", page: Page(story: .Droid))
-        */
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "startAdventure" {
+            if let pageController = segue.destinationViewController as? PageController {
+                pageController.page = Adventure.story
+            }
+        }
     }
 }
